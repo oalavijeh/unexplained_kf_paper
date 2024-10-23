ebcee07f938a7b453c38c1a265706ff9e1e475a4
oalavijeh <56344383+oalavijeh@users.noreply.github.com>
2024-10-23 14:01:38 +0100

    regression_and_comparison_for_export.R

--- regression_and_comparison_for_export.R
+++ regression_and_comparison_for_export.R
@@ -19,7 +19,7 @@ options(scipen=0)
 
 # Calculate liability threshold heritability
 #K=pop prevalence
-#P=proportion of cases in study 1209/26096
+#P=proportion of cases in study 
 #hsq=Heritability estimate (on observed scale)
 #bigT = liability threshold
 #tau = density of gaussian
@@ -44,7 +44,6 @@ colnames(ssns_prs)[3] <- "ssns_prs"
 membranous_prs <- fread("membranous/unexplained_membranous_prs_scores.sscore")
 colnames(membranous_prs)[1] <- "id"
 colnames(membranous_prs)[3] <- "membranous_prs"
-barry_ssns <- fread("unexplained_esrf/prs/ssns/barry_ssns_unexplained_esrf.best")
 # 
 # #merge all 
 prs <- Reduce(merge,list(ckd_prs, iga_prs[,c(1,3)],membranous_prs[,c(1,3)],ssns_prs[,c(1,3)]))
@@ -141,14 +140,6 @@ CI_ssns<- exp(confint(model_ssns, level=0.95)["ssns_prs_standardised",])
 CI_lower_ssns <- CI_ssns[1]
 CI_upper_ssns <- CI_ssns[2]
 
-#ma_ssns
-model_ma_ssns <- glm(pheno~ma_ssns +pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+sex+age, data=prs, family = 'binomial')
-summary_ma_ssns <- summary(model_ma_ssns)
-OR_ma_ssns<-exp(coef(summary_ma_ssns)["ma_ssns","Estimate"])
-CI_ma_ssns<- exp(confint(model_ma_ssns, level=0.95)["ma_ssns",])
-CI_lower_ma_ssns <- CI_ma_ssns[1]
-CI_upper_ma_ssns <- CI_ma_ssns[2]
-
 #membranous
 model_membranous <- glm(pheno~membranous_prs_standardised +pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+sex+age, data=prs, family = 'binomial')
 summary_membranous <- summary(model_membranous)
@@ -175,10 +166,10 @@ CI_upper_monogenic <- CI_monogenic[2]
 
 #summary_data - with monogenic
 summary_stats <- data.frame(
-  Variable=c("APOL1","Monogenic","IgA PRS","SSNS PRS","maSSNS PRS", "Membranous PRS", "CKD PRS"),
-  OR=c(OR_apol1,OR_monogenic, OR_iga, OR_ssns, OR_ma_ssns, OR_membranous,OR_ckd),
-  CI_Lower=c(CI_lower_apol1,CI_lower_monogenic, CI_lower_iga, CI_lower_ssns,CI_lower_ma_ssns, CI_lower_membranous,CI_lower_ckd),
-  CI_Upper=c(CI_upper_apol1, CI_upper_monogenic,CI_upper_iga, CI_upper_ssns,CI_upper_ma_ssns, CI_upper_membranous,CI_upper_ckd)
+  Variable=c("APOL1","Monogenic","IgA PRS","SSNS PRS","Membranous PRS", "CKD PRS"),
+  OR=c(OR_apol1,OR_monogenic, OR_iga, OR_ssns, OR_membranous,OR_ckd),
+  CI_Lower=c(CI_lower_apol1,CI_lower_monogenic, CI_lower_iga, CI_lower_ssns, CI_lower_membranous,CI_lower_ckd),
+  CI_Upper=c(CI_upper_apol1, CI_upper_monogenic,CI_upper_iga, CI_upper_ssns,CI_upper_membranous,CI_upper_ckd)
   )
 
 #write.table(summary_stats, "with_age_summary_stats_for_prs_analysis_for_forest_monogenic.txt", col.names = T, row.names = F, quote = F, sep ='\t')
@@ -237,25 +228,13 @@ african_prs$apol1_type <- ifelse(african_prs$type.y=="aa_esrf" & african_prs$apo
 african_prs$apol1_type <- ifelse(african_prs$type.y=="aa_control" & african_prs$apol1==1, "Control APOL1",african_prs$apol1_type)
 african_prs$apol1_type <- ifelse(african_prs$type.y=="aa_control" & african_prs$apol1==0, "Control",african_prs$apol1_type)
 
-ggplot(african_prs, aes(x = ma_ssns, fill = apol1_type)) +
-  geom_density(alpha = 0.2) +
-  theme_classic() +
-  labs(x = "PRS", y = "Density", fill = "Cohort") +
-  facet_grid(rows = vars(apol1_type))
-
 for_plotting <- subset(african_prs, african_prs$apol1_type=="Case APOL1" | 
                          african_prs$apol1_type=="Control APOL1" |
                          african_prs$apol1_type=="Control")
 
-ggplot(for_plotting, aes(x = ma_ssns, fill = apol1_type)) +
-  geom_density(alpha = 0.2) +
-  theme_classic() +
-  labs(x = "PRS", y = "Density", fill = "Cohort") 
-  #facet_grid(rows = vars(apol1_type))
-
 
 my_comparisons <- list(c("Case APOL1","Control APOL1"),c("Case APOL1","Control"),c("Control", "Control APOL1"))
-ggviolin(for_plotting, x = "apol1_type", y = "ma_ssns", fill = "apol1_type",
+ggviolin(for_plotting, x = "apol1_type", y = "sns", fill = "apol1_type",
          palette = c("#00AFBB", "#FC4E07","#f2241f"),
          add = "boxplot", add.params = list(fill = "white"))+
   stat_compare_means(comparisons = my_comparisons, label = "p.signif")# Add significance levels
@@ -263,8 +242,8 @@ stat_compare_means(label.y = 8) +
   #stat_cor(p.accuracy = 0.01) +
   labs(x="Cohort", y="Polygenic Risk Score", fill="Cohort") 
 
-kruskal.test(ma_ssns ~ apol1_type, data=for_plotting)
-pairwise.wilcox.test(for_plotting$ma_ssns, for_plotting$apol1_type, p.adjust.method = "BH")
+kruskal.test(ssns ~ apol1_type, data=for_plotting)
+pairwise.wilcox.test(for_plotting$ssns, for_plotting$apol1_type, p.adjust.method = "BH")
 
 kruskal.test(ssns_prs_standardised ~ apol1_type, data=for_plotting)
 pairwise.wilcox.test(for_plotting$ssns_prs_standardised, for_plotting$apol1_type, p.adjust.method = "BH")
@@ -316,11 +295,11 @@ ggplot(df_summary, aes(x = PRS_score, y = mean, color = cohort)) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "black")
 
 
-#with barry ssns
+#with ssns
 prs2 <- prs
-prs2$ma_ssns <- prs2$ma_ssns*-1
+prs2$ssns <- prs2$ssns*-1
 df_long <- prs2 %>%
-  pivot_longer(cols = c("iga_prs_standardised", "ssns_prs_standardised", "ma_ssns",
+  pivot_longer(cols = c("iga_prs_standardised", "ssns_prs_standardised",
                         "membranous_prs_standardised", "ckd_apol_adjusted"), 
                names_to = "PRS_score", values_to = "PRS_value") %>%
   mutate(cohort = case_when(
@@ -339,7 +318,7 @@ df_summary <- df_long %>%
     upper_ci = mean + qt(0.975, df=n()-1) * sd(PRS_value) / sqrt(n()),
     .groups = 'drop'
   )
-  df_summary$PRS_score <- factor(df_summary$PRS_score, levels = c("ma_ssns","ssns_prs_standardised",
+  df_summary$PRS_score <- factor(df_summary$PRS_score, levels = c("ssns_prs_standardised",
                                                           "membranous_prs_standardised",
                                                           "ckd_apol_adjusted","iga_prs_standardised"))
 
@@ -353,7 +332,6 @@ ggplot(df_summary, aes(x = PRS_score, y = mean, color = cohort)) +
   theme_minimal() +
   scale_x_discrete(labels = c("iga_prs_standardised" = "IgA PRS", 
                               "ssns_prs_standardised" = "SSNS PRS",
-                              "ma_ssns" = "MA-SSNS PRS",
                               "membranous_prs_standardised" = "Membranous PRS",
                               "ckd_apol_adjusted" = "CKD PRS")) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -371,7 +349,6 @@ library(multcomp)
 # Subset data for one PRS type, for example, Iga
 df_iga <- df_long %>% filter(PRS_score == "iga_prs_standardised")
 df_ssns <- df_long %>% filter(PRS_score == "ssns_prs_standardised")
-df_ssns_ma <- df_long %>% filter(PRS_score == "ma_ssns")
 df_ckd <- df_long %>% filter(PRS_score == "ckd_apol_adjusted")
 df_membranous <- df_long %>% filter(PRS_score == "membranous_prs_standardised")
 
@@ -379,8 +356,6 @@ df_membranous <- df_long %>% filter(PRS_score == "membranous_prs_standardised")
 anova_result <- aov(PRS_value ~ cohort, data = df_iga)
 summary(anova_result)
 anova_result <- aov(PRS_value ~ cohort, data = df_ssns)#significant for APOL1
-summary(anova_result)
-anova_result <- aov(PRS_value ~ cohort, data = df_ssns_ma)#significant for APOL1
 summary(anova_result)
 anova_result <- aov(PRS_value ~ cohort, data = df_ckd)
 summary(anova_result)
@@ -404,19 +379,6 @@ if (summary(anova_result)[[1]]$'Pr(>F)'[1] < 0.05) {
 # Repeat the process for each PRS type
   
 ###african mean prs comparison
-  #with barry ssns
-  african_prs$ma_ssns <-  african_prs$ma_ssns*-1
-  df_long <- african_prs %>%
-    pivot_longer(cols = c("iga_prs_standardised", "ssns_prs_standardised", "ma_ssns",
-                          "membranous_prs_standardised", "ckd_apol_adjusted"), 
-                 names_to = "PRS_score", values_to = "PRS_value") %>%
-    mutate(cohort = case_when(
-      pheno == 1 & apol1 == 1 ~ "APOL1 (n=16)", # APOL1_Positive takes priority
-      pheno == 0 & apol1 == 1 ~ "APOL1 Controls (n=51)",
-      pheno == 0 & apol1 == 0 ~ "AA ancestry Controls (n=199)", TRUE ~ as.character(NA)
-    ))%>%
-    filter(cohort %in% c("APOL1 (n=16)", "APOL1 Controls (n=51)", "AA ancestry Controls (n=199)")) # Filter to include only the specified cohorts
-  
   # Calculate means and confidence intervals
   df_summary <- df_long %>%
     group_by(PRS_score, cohort) %>%
@@ -426,12 +388,11 @@ if (summary(anova_result)[[1]]$'Pr(>F)'[1] < 0.05) {
       upper_ci = mean + qt(0.975, df=n()-1) * sd(PRS_value) / sqrt(n()),
       .groups = 'drop'
     )
-  df_summary$PRS_score <- factor(df_summary$PRS_score, levels = c("ma_ssns","ssns_prs_standardised",
+  df_summary$PRS_score <- factor(df_summary$PRS_score, levels = c("ssns_prs_standardised",
                                                                   "membranous_prs_standardised",
                                                                   "ckd_apol_adjusted","iga_prs_standardised"))
  #plot without non ssns scores
-  df_summary <- subset(df_summary, df_summary$PRS_score=="ma_ssns"|
-                         df_summary$PRS_score=="ssns_prs_standardised")
+  df_summary <- subset(df_summary, df_summary$PRS_score=="ssns_prs_standardised")
   # Plotting
   
   ggplot(df_summary, aes(x = PRS_score, y = mean, color = cohort)) +
@@ -442,7 +403,6 @@ if (summary(anova_result)[[1]]$'Pr(>F)'[1] < 0.05) {
     theme_minimal() +
     scale_x_discrete(labels = c("iga_prs_standardised" = "IgA PRS", 
                                 "ssns_prs_standardised" = "SSNS PRS",
-                                "ma_ssns" = "MA-SSNS PRS",
                                 "membranous_prs_standardised" = "Membranous PRS",
                                 "ckd_apol_adjusted" = "CKD PRS")) +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -460,7 +420,6 @@ if (summary(anova_result)[[1]]$'Pr(>F)'[1] < 0.05) {
   # Subset data for one PRS type, for example, Iga
   df_iga <- df_long %>% filter(PRS_score == "iga_prs_standardised")
   df_ssns <- df_long %>% filter(PRS_score == "ssns_prs_standardised")
-  df_ssns_ma <- df_long %>% filter(PRS_score == "ma_ssns")
   df_ckd <- df_long %>% filter(PRS_score == "ckd_apol_adjusted")
   df_membranous <- df_long %>% filter(PRS_score == "membranous_prs_standardised")
   
@@ -468,8 +427,6 @@ if (summary(anova_result)[[1]]$'Pr(>F)'[1] < 0.05) {
   anova_result <- aov(PRS_value ~ cohort, data = df_iga)
   summary(anova_result)
   anova_result <- aov(PRS_value ~ cohort, data = df_ssns)#significant for APOL1
-  summary(anova_result)
-  anova_result <- aov(PRS_value ~ cohort, data = df_ssns_ma)#significant for APOL1
   summary(anova_result)
   anova_result <- aov(PRS_value ~ cohort, data = df_ckd)
   summary(anova_result)
@@ -504,13 +461,7 @@ if (summary(anova_result)[[1]]$'Pr(>F)'[1] < 0.05) {
     prs_analysis$cohort <- as.factor(prs_analysis$cohort)
     prs_analysis$sex <- as.factor(prs_analysis$sex)
     
-#model ma_ssns
-model <- lm(ma_ssns ~ cohort + sex + age + pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10, data = prs_analysis)
-    summary(model)
-    par(mfrow=c(2,2))
-    plot(model)
-    prs_analysis$predicted_ma_ssns <- predict(model)
-    
+#model 
 model <- lm(ckd_apol_adjusted ~ cohort + sex + age + pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+cohort:ckd_apol_adjusted, data = prs_analysis)
     summary(model)
     par(mfrow=c(2,2))
@@ -740,129 +691,3 @@ ggviolin(prs, x = "pheno", y = "membranous_prs_standardised", fill = "pheno",
   #stat_cor(p.accuracy = 0.01) +
   labs(x="Cohort", y="Polygenic Risk Score", fill="Cohort") 
 ggsave("slc34a3_subgroup_prs_stones_ggpubr_4groups.violin.png", height = 7, width = 10)
-
-
-#power calculations 
-n <- 25304
-alpha <- 0.05
-OR_interact <- 2
-power <- pwr.f2.test(u=log(OR_interact), v1 =15,v2=25289, sig.level = alpha, power=NULL)
-
-###all commerts 
-prs_plotting <- prs
-prs_plotting$rank <- rank(prs_plotting$prs_standardised)
-prs_plotting$rank <- round(prs_plotting$rank/length(prs_plotting$rank) * 100)
-x <- prs_plotting %>% group_by(rank) %>% mutate(prevelance = (sum(pheno==1)/length(pheno)*100))
-l <- x[,c(18,19)]
-l <- unique(l)
-l <- l[order(l$rank,decreasing = F),]
-ggplot(l, aes(x=rank, y=prevelance)) + geom_smooth(method="glm", alpha=.15)
-#fit model with rank 
-unsolved_plotting_centile <- unsolved_plotting[,c(1,14,3:13)]
-unsolved_centile <- glm(PHENO~., family=binomial(link='logit'),data=unsolved_plotting_centile, maxit=10000)
-unsolved_plot<-effect_plot(data = unsolved_plotting_centile, unsolved_centile, pred=rank, interval = F, x.label = "PRS Centile",
-                           y.label = "Prevelance of USD") + scale_y_continuous(labels = scales::percent)
-
-#combine data for ggplot
-data1 <- make_predictions(unsolved_centile, pred="rank")
-data1$model <- "SLC34A3 non-carrier"
-data2 <- make_predictions(slc34a3_centile, pred="rank")
-data2$model <- "SLC34A3 carrier"
-cdata <- bind_rows(data1,data2)
-
-ggplot(data = cdata,aes(x=rank,y=PHENO,color=model)) +
-  geom_smooth(method = "glm", alpha=0.15, se = T) + 
-  labs(x = "Percentile of Polygenic risk score", y = "Prevelance of USD", color = "Model") +
-  scale_y_continuous(labels = scales::percent, limits = c(0,0.02)) +
-  theme_minimal() + 
-  scale_color_discrete(name = "Cohort") 
-
-
-ggplot() +
-  geom_line(data=data1, aes(x=rank, y=PHENO), color='green') + geom_smooth(method="glm", alpha=.15) +
-  geom_line(data=data2, aes(x=rank, y=PHENO), color='red') + geom_smooth(method="glm", alpha=.15) +
-  scale_y_continuous(labels = scales::percent, limits = c(0,0.02)) 
-
-
-###########probability plot#######
-# Create a new column for prevalence by cohort
-df <- prs
-df$prevalence <- ifelse(df$pheno == 1, 1, 0)
-prevalence_by_cohort <- aggregate(prevalence ~ prs_standardised + type, df, mean)
-prevalence_by_cohort$prevalence <- prevalence_by_cohort$prevalence * 100
-# Create the plot - facet
-ggplot(df, aes(x = prs_standardised, y = prevalence, color = type)) + 
-  #geom_point() + 
-  geom_smooth(method = "glm", se = FALSE, span = 0.5) +
-  xlab("PRS") + 
-  ylab("Prevalence") + 
-  ggtitle("Prevalence of phenotype by PRS and cohort") +
-  facet_wrap(~pheno, scales = "free_x")
-
-#single plot
-ggplot(df, aes(x = prs_standardised, y = prevalence, color = pheno, group = pheno)) + 
-  #geom_point() + 
-  geom_smooth(method = "glm", se = FALSE, span = 0.5) +
-  xlab("PRS") + 
-  ylab("Proportion with USD in 100KGP") + 
-  labs(color="Carrier Status")+
-  scale_y_continuous(labels = scales::percent) + 
-  theme_minimal()
-ggsave("analysis/proportion_with_usd_plot_100kgp.png", height = 7, width = 7)
-
-#distrbutions for presentation 
-dist <- pheno.prs
-
-#plot
-ggplot(dist, aes(x=prs_standardised, fill=pheno))+
-  geom_density(alpha=0.2)+
-  theme_classic()+
-  labs(x="Polygenic Score", y="Density", fill="Cohort") + facet_grid(rows = vars(pheno)) +
-  geom_vline(xintercept = 0, linetype = "dotted")
-
-
-ggsave("densities_slc34a3_case_unsolved_cases_controls.density.png", height = 7, width = 7)
-
-ggplot(dist, aes(x = prs_standardised, fill = pheno)) +
-  geom_density(alpha = 0.2) +
-  geom_vline(data = pheno.prs, 
-             aes(xintercept = median_prs, group = pheno), 
-             color = "black", 
-             size = 1, linetype = "dotted") +
-  theme_classic() +
-  labs(x = "PRS", y = "Density", fill = "Cohort") +
-  facet_grid(rows = vars(pheno))
-
-ggsave("analysis/densities_slc34a3_case_unsolved_cases_controls.density_median_line.png", height = 7, width = 7)
-
-
-#reshape
-library(reshape2)
-to_melt <- prs[,c(3,19:22)]
-melted_df <- melt(to_melt, id.vars="pheno")
-ggplot(melted_df, aes(x=variable, y=value, fill=factor(pheno))) +
-  geom_violin(scale="width", draw_quantiles = c(0.25,0.5,0.75)) +
-  geom_boxplot(width=0.1, position = position_dodge(0.9), alpha=0.5) +
-  labs(x="Polygenic score type", y="PRS", fill = "pheno") + 
-  theme_minimal() + 
-  theme(legend.position = "top") +
-  scale_fill_manual(values = c("0" = "blue", "1" = "red"))
-
-to_melt <- prs[,c(3,19:22)]
-melted_df <- melt(to_melt, id.vars="pheno")
-ggplot(melted_df, aes(x=variable, y=value, color=factor(pheno))) +
-  geom_line() +
-  labs(x="Polygenic score type", y="PRS") + 
-  theme_minimal() 
-
-phenotype_0 <- melted_df[melted_df$pheno==0,]
-phenotype_1 <- melted_df[melted_df$pheno==1,]
-
-
-plot_0 <- ggplot(phenotype_0, aes(x=value, factor(variable))) +
-  geom_density(alpha=0.5) +
-  labs(x="PRS", y="Density", fill = "PRS") + 
-  theme_minimal() 
-
-ggplot(melted_df, aes(x=variable,y=value, group=variable))+
-  geom_density_ridges2()
